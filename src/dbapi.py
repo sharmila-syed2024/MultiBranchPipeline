ee44cd60f28d7251d5ffeb4bf025e7784cca36ee
muyiwao <muyiwa.oladimeji@yahoo.com>
2024-08-08 10:42:51 +0100

    update files

--- src/dbapi.py
+++ src/dbapi.py
@@ -3,17 +3,31 @@ import pandas as pd
 from sqlalchemy import create_engine
 from flask import Flask, jsonify
 import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+# Function to get environment variables with error handling
+def get_env_variable(var_name):
+    value = os.getenv(var_name)
+    if value is None:
+        raise EnvironmentError(f"Environment variable {var_name} not set.")
+    return value
+
+
 # Define your PostgreSQL connection parameters
 DATABASE_TYPE = 'postgresql'
 DBAPI = 'psycopg2'
-ENDPOINT = os.environ['ENDPOINT']  # os.getenv('ENDPOINT')
-USER = os.environ['USER']
-PASSWORD = os.environ['PASSWORD']
-PASSWORD = quote_plus(str(PASSWORD))
-PORT = os.environ['PORT']
-DATABASE = os.environ['DATABASE']
+ENDPOINT = get_env_variable('ENDPOINT')
+USER = get_env_variable('USER')
+PASSWORD = get_env_variable('PASSWORD')
+PASSWORD = quote_plus(PASSWORD)  # Ensure PASSWORD is a string before quoting
+PORT = get_env_variable('PORT')
+DATABASE = get_env_variable('DATABASE')
 
 # Create a SQLAlchemy engine
 engine = create_engine(f'{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}')
@@ -35,4 +49,4 @@ def get_data():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5300, debug=True)
